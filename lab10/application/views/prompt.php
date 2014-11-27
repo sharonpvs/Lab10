e08831f79ed4b3f37bbca616a970285bdadc3d56
Okitora <chris.m.c.lee@hotmial.com>
2014-11-27 15:14:17 -0800

    ????confused

--- lab10/application/views/prompt.php
+++ lab10/application/views/prompt.php
@@ -1,7 +1,6 @@
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5477a9c4105bf1c6" async="async"></script>
 
-
 <form action="/planner/tripresult/" method="post">
     {leaving}
     {destination}
